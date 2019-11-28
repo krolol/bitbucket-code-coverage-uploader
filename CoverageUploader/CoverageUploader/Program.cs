c4f34da91901f75faafde58af5f5fe6508ff4a6f
oyankovich <oyankovich@aras.com>
2019-11-29 01:48:25 +0300

    Add function to send requests to Client. Add parsing for Arguments

--- CoverageUploader/CoverageUploader/Program.cs
+++ CoverageUploader/CoverageUploader/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using CommandLine;
+using CoverageUploader.Rest;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CoverageUploader
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public class Arguments
+	{
+		[Option('u', "bitbucket.username", Required = true, HelpText = "Username of bitbucket user")]
+		public string Username { get; set; }
+		[Option('p', "bitbucket.password", Required = true, HelpText = "Password of bitbucket user")]
+		public string Password { get; set; }
+		[Option('c', "bitbucket.commit.id", Required = true, HelpText = "Commit id")]
+		public string CommidID { get; set; }
+		[Option('b', "bitbucket.url", Required = true, HelpText = "Bitbucket url")]
+		public string BitbucketURL { get; set; }
+		[Option('f', "coverage.file", Required = true, HelpText = "Path to coverage file")]
+		public string CoverageFile { get; set; }
+
+	}
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			Parser.Default.ParseArguments<Arguments>(args)
+				.WithParsed(arguments => RunArguments(arguments));
+			return -1;
+		}
+
+		private static int RunArguments(Arguments arguments)
+		{
+			try
+			{
+				var client = new Client(arguments.BitbucketURL, arguments.Username, arguments.Password);
+				var responseMessage = client.PostCoverageAsync(arguments.CommidID, "{\"file\"}");
+				Console.WriteLine(responseMessage.Result.ToString());
+				return 0;
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e);
+				return -1;
+			}
+		}
+	}
 }
--- CoverageUploader/CoverageUploader/Rest/Client.cs
+++ CoverageUploader/CoverageUploader/Rest/Client.cs
@@ -1,44 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CoverageUploader.Rest
 {
 	class Client
 	{
-		private string _bitbucketURL
-		{
-			set
-			{
-				if (string.IsNullOrEmpty(value))
-				{
-					throw new ArgumentException("Bitbucket URL cannot be empty");
-				}
-				_bitbucketURL = value;
-			}
-		}
-		private string _user 
-		{ 
-			set
-			{
-				if (string.IsNullOrEmpty(value))
-				{
-					throw new ArgumentException("User cannot be empty");
-				}
-				_user = value;
-			}
-		}
+		private string _bitbucketURL { get; set; }
+		private string _username { get; set; }
 		private string _password { get; set; }
-		private string _token { get; set; }
-		private int? _timeout { get; set; }
+		private int _timeout { get; set; }
 
-		public Client(string bitbucketURL, string user, string password, string token, int? timeout = 100)
+		public Client(string bitbucketURL, string username, string password, int timeout = 120)
 		{
 			_bitbucketURL = bitbucketURL;
-			_user = user;
+			_username = username;
 			_password = password;
-			_token = token;
 			_timeout = timeout;
+		}
+
+		public async Task<HttpResponseMessage> PostCoverageAsync(string commitId, string jsonData)
+		{
+			using (var client = new HttpClient())
+			{
+				var requestURL = $"{_bitbucketURL}/rest/code-coverage/1.0/commits/{commitId}";
+				var data = new StringContent(jsonData, Encoding.UTF8, "application/json");
+				var authToken = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_username}:{_password}"));
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
+				client.Timeout = TimeSpan.FromSeconds(_timeout);
+				var response = await client.PostAsync(requestURL, data);
+				return response;
+			}
 		}
 	}
 }
