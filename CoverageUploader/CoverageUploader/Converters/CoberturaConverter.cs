06cc62bcb69423a498020877fc0821e0f20d023e
Oleg Yankovich <oyankovich@aras.com>
2019-12-02 10:57:42 +0300

    Add supporting source tag

--- CoverageUploader/CoverageUploader/Converters/CoberturaConverter.cs
+++ CoverageUploader/CoverageUploader/Converters/CoberturaConverter.cs
@@ -1,6 +1,8 @@
 ﻿using CoverageUploader.Coverage;
 using System.Xml;
 using LibGit2Sharp;
+using System.IO;
+using System;
 
 namespace CoverageUploader.Converters
 {
@@ -8,15 +10,19 @@ namespace CoverageUploader.Converters
 	{
 		private const string _pathToClass = "//coverage/packages/package/classes/class";
 		private const string _pathToLine = "lines/line";
+		private const string _pathToSource = "coverage/sources/source";
 
 		public static JSONCoverage ConvertFromCobertura(string pathToCoverageFile)
 		{
 			JSONCoverage jsonCoverage = new JSONCoverage();
 			XmlDocument xmlDocument = new XmlDocument();
 			xmlDocument.Load(pathToCoverageFile);
+			XmlNode sourceNode = xmlDocument.SelectSingleNode(_pathToSource);
+			string source = string.Equals(sourceNode.Value, null) ? string.Empty : sourceNode.Value;
 			foreach (XmlNode xmlNode in xmlDocument.SelectNodes(_pathToClass))
 			{
-				string fileName = Help.GetRelativeFilePath(xmlNode.Attributes["filename"].Value);
+				Console.WriteLine(Path.Combine(source, xmlNode.Attributes["filename"].Value));
+				string fileName = Help.GetRelativeFilePath(Path.Combine(source, xmlNode.Attributes["filename"].Value));
 				FileCoverage fileCoverage = new FileCoverage(fileName);
 				var a = xmlNode.SelectNodes(_pathToLine).Count;
 				foreach (XmlNode node in xmlNode.SelectNodes(_pathToLine))
